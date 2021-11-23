2feeef3044ccefc17a0cc0f4ef7f443b430dbb7b
Alex Lam <alexlamzing@gmail.com>
2021-11-24 01:36:26 +0800

    chore: remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import styled from 'styled-components'
 
 import { TemperatureList } from './components/TemperatureList'
